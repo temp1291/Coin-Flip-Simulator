dff4f7222bec7504b75ebb19e4fe4708bce16a8c
max <thefpgfake@ukr.net>
2024-08-06 21:50:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+def main():
+    while True:
+        input('Press Enter to flip a coin...')
+        result = flip_coin()
+        print(result)
+
+
+def flip_coin():
+    result = randint(0, 1)
+    if result:
+        return 'It landed heads.'
+    else:
+        return 'It landed tails.'
+
+
+if __name__ == '__main__':
+    main()
